c93761d0662403f839b488911dc84470fd1f8c4c
Luis E. Sánchez P <lespinerua@gmail.com>
2022-03-21 00:42:43 -0400

    Favorite List and Icons

--- src/screens/Favorites.js
+++ src/screens/Favorites.js
@@ -1,22 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { getPokemonsFavoriteApi } from '../api/favorite';
+import { getPokemonDetailsByIdApi, getPokemonDetailsByUrlApi } from '../api/pokemon';
+import PokemonList from '../components/PokemonList';
 
 export default function Favorite() {
 
-    const [favorites, setFavorites] = useState([])
+    const [pokemons, setPokemons] = useState([])
 
-    useEffect(() => {
+    useFocusEffect(() => {
         (async () => {
             const response = await getPokemonsFavoriteApi()
-            console.log(response)
+            const pokemonsArray = []
+
+            for (const pokemonId of response) {
+                const pokemonDetails = await getPokemonDetailsByIdApi(pokemonId)
+                pokemonsArray.push({
+                    id: pokemonDetails.id,
+                    name: pokemonDetails.name,
+                    type: pokemonDetails.types[0].type.name,
+                    order: pokemonDetails.order,
+                    image: pokemonDetails.sprites.other['official-artwork'].front_default
+                })
+            }
+            setPokemons(pokemonsArray)
+
         })();
 
-    }, [])
+
+    })
+
     return (
         <SafeAreaView>
-            <Text>Favorite</Text>
+            {/* <Text>Favorites</Text> */}
+            <PokemonList pokemons={pokemons} />
         </SafeAreaView>
     );
 }
